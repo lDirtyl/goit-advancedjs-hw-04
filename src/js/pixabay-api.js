b9ce51d3041ec08f5264946b5e589b96a6e32ec5
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2024-10-29 17:50:56 +0100

    main.js and pixabay.js finish

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,34 @@
+import axios from 'axios';
+
 const API_KEY = '46747450-fc08b8bc876855aa84ec0c871';
 const URL = 'https://pixabay.com/api/?';
 
-export function fetchImages(query) {
-  const searchParams = new URLSearchParams({
-    key: API_KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
+export function fetchImages(query, currentPage) {
+  const axiosConfigs = {
+    params: {
+      key: API_KEY,
+      q: query,
+      page: currentPage,
+      per_page: 15,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  };
+  return axios.get(`${URL}`, axiosConfigs);
 
-  return fetch(`${URL}${searchParams}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  // const searchParams = new URLSearchParams({
+  //   key: API_KEY,
+  //   q: query,
+  //   image_type: 'photo',
+  //   orientation: 'horizontal',
+  //   safesearch: true,
+  // });
+
+  // return fetch(`${URL}${searchParams}`).then(response => {
+  //   if (!response.ok) {
+  //     throw new Error(response.status);
+  //   }
+  //   return response.json();
+  // });
 }
--- src/main.js
+++ src/main.js
@@ -8,8 +8,31 @@ import { renderGalleryCardTemplate } from './js/render-functions';
 const formEl = document.querySelector('.search-form');
 const galleryListEl = document.querySelector('.gallery');
 const loaderEl = document.querySelector('.js-loader');
+const loadMoreBtnEl = document.querySelector('.js-load-more');
 
 let lightbox;
+let currentPage = 1;
+let searchedValue = '';
+const limit = 15;
+
+const getGalleryCardHeight = () => {
+  const firstCard = galleryListEl.querySelector('.gallery-card');
+  if (firstCard) {
+    const { height } = firstCard.getBoundingClientRect();
+    return height;
+  }
+  return 0;
+};
+
+const scrollToNextImages = () => {
+  const cardHeight = getGalleryCardHeight();
+  if (cardHeight > 0) {
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  }
+};
 
 const initLightbox = () => {
   lightbox = new SimpleLightbox('.gallery a', {
@@ -24,54 +47,108 @@ const refreshLightbox = () => {
   }
 };
 
-const onSearchFormSubmit = event => {
-  event.preventDefault();
+const onSearchFormSubmit = async event => {
+  try {
+    event.preventDefault();
 
-  const searchedValue = event.target.elements.searchQuery.value
-    .toLowerCase()
-    .trim();
+    searchedValue = event.target.elements.searchQuery.value
+      .toLowerCase()
+      .trim();
 
-  if (searchedValue === '') {
-    iziToast.error({
-      message: 'The search field should not be empty!',
-      position: 'topRight',
-    });
+    currentPage = 1;
 
-    formEl.reset();
-    galleryListEl.innerHTML = '';
-    return;
-  }
+    if (searchedValue === '') {
+      galleryListEl.innerHTML = '';
+      iziToast.error({
+        message: 'The search field should not be empty!',
+        position: 'topRight',
+      });
 
-  loaderEl.classList.remove('is-hidden');
-
-  fetchImages(searchedValue)
-    .finally(() => {
-      loaderEl.classList.add('is-hidden');
-    })
-    .then(({ hits }) => {
-      if (hits.length === 0) {
-        galleryListEl.innerHTML = '';
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-        });
-
-        formEl.reset();
-        return;
-      }
-
-      const galleryCardsTemplate = renderGalleryCardTemplate(hits);
-      galleryListEl.innerHTML = galleryCardsTemplate;
       formEl.reset();
 
-      if (!lightbox) {
-        initLightbox();
-      } else {
-        refreshLightbox();
-      }
-    })
-    .catch(error => console.log(error));
+      loadMoreBtnEl.classList.add('is-hidden');
+      return;
+    }
+
+    loaderEl.classList.remove('is-hidden');
+
+    const { data } = await fetchImages(searchedValue, currentPage);
+
+    loaderEl.classList.add('is-hidden');
+
+    if (data.hits.length === 0) {
+      galleryListEl.innerHTML = '';
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
+
+      formEl.reset();
+      loadMoreBtnEl.classList.add('is-hidden');
+      return;
+    }
+
+    const galleryCardsTemplate = renderGalleryCardTemplate(data.hits);
+    galleryListEl.innerHTML = galleryCardsTemplate;
+    formEl.reset();
+
+    if (data.totalHits <= limit) {
+      iziToast.warning({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+      });
+    } else {
+      loadMoreBtnEl.classList.remove('is-hidden');
+    }
+
+    if (!lightbox) {
+      initLightbox();
+    } else {
+      refreshLightbox();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const onLoadMoreBtnClick = async event => {
+  try {
+    currentPage += 1;
+
+    loadMoreBtnEl.classList.add('is-hidden');
+    loaderEl.classList.remove('is-hidden');
+
+    const { data } = await fetchImages(searchedValue, currentPage);
+
+    loaderEl.classList.add('is-hidden');
+
+    const totalPages = Math.ceil(data.totalHits / limit);
+
+    if (currentPage >= totalPages) {
+      galleryListEl.innerHTML = '';
+      iziToast.warning({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+      });
+    } else {
+      loadMoreBtnEl.classList.remove('is-hidden');
+    }
+
+    const galleryCardsTemplate = renderGalleryCardTemplate(data.hits);
+    galleryListEl.insertAdjacentHTML('beforeend', galleryCardsTemplate);
+
+    if (!lightbox) {
+      initLightbox();
+    } else {
+      refreshLightbox();
+    }
+
+    scrollToNextImages();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 formEl.addEventListener('submit', onSearchFormSubmit);
+loadMoreBtnEl.addEventListener('click', onLoadMoreBtnClick);
